a4217ac9c0ba8da76cde2914abfb619909176a71
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-12 15:25:36 +0800

    简单的controller

--- src/main/java/com/honthion/eleusine/Application.java
+++ src/main/java/com/honthion/eleusine/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EleusineApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EleusineApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/honthion/eleusine/services/OrganizationService.java
@@ -0,0 +1,7 @@
+package com.honthion.eleusine.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationService {
+}
