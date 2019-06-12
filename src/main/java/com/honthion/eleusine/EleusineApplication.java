c65e71c93b36ab4e370c928534ed85030397118d
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-12 14:59:58 +0800

    init

---
+++ src/main/java/com/honthion/eleusine/EleusineApplication.java
@@ -0,0 +1,13 @@
+package com.honthion.eleusine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleusineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleusineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honthion/eleusine/EleusineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.honthion.eleusine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EleusineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
